b51a4f4663d56f41ace8baf02aec7f7cf0f9004b
nicoletanyt <nicole_tan_yitong@s2021.ssts.edu.sg>
2023-03-13 23:17:56 +0800

    Finished Sign-Up Page UI

--- root/src/App.js
+++ root/src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import Signup from "./SIGN-UP/Signup";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Signup></Signup>
+    </div>
+  );
 }
 
 export default App;
