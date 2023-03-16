5a54ccd09044f8bef7e74bd2cd813673a1fe20a2
nicoletanyt <nicole_tan_yitong@s2021.ssts.edu.sg>
2023-03-16 15:49:45 +0800

    added login function and backend code

---
+++ root/index.js
@@ -0,0 +1,27 @@
+const PORT = 8000;
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+app.use(cors());
+
+// app.get("/", (req, res) => {
+//   res.json("hi");
+// });
+
+app.get("/config", (req, res) => {
+  const config = {
+    apiKey: process.env.REACT_APP_APIKEY,
+    authDomain: process.env.REACT_APP_AUTHDOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APPID,
+    measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+    databaseURL: process.env.REACT_APP_DATABASE_URL,
+  };
+  res.json(config);
+});
+
+app.listen(8000, () => console.log(`Server is running on port ${PORT}`));
--- root/src/PAGES/SignupPage.js
+++ root/src/PAGES/SignupPage.js
@@ -1,15 +1,45 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "../STYLES/Signup.css";
 import { useNavigate } from "react-router-dom";
+import * as firebase from "firebase/app";
+import axios from "axios";
+import "firebase/auth";
 
 //FIREBASE IMPORTS
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
-import { getDatabase, set, ref } from "firebase/database";
-import app from "../FIREBASE/Base";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import { getDatabase, set, ref, update } from "firebase/database";
+// import app from "../FIREBASE/Base";
 
 export default function SignupPage() {
-  const auth = getAuth(app);
-  const database = getDatabase(app);
+  const [app, setApp] = useState(null);
+  const [auth, setAuth] = useState(null);
+  const [database, setDatabase] = useState(null);
+
+  useEffect(() => {
+    axios({
+      method: "get",
+      url: "http://localhost:8000/config",
+    })
+      .then(function (response) {
+        // console.log(response.data);
+        const firebaseApp = firebase.initializeApp(response.data);
+        setApp(firebaseApp);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  useEffect(() => {
+    if (app) {
+      setAuth(getAuth(app));
+      setDatabase(getDatabase(app));
+    }
+  }, [app]);
   const navigate = useNavigate();
 
   const [email, setEmail] = useState("");
@@ -51,34 +81,47 @@ export default function SignupPage() {
         <button
           id="submit_btn"
           onClick={() => {
-            createUserWithEmailAndPassword(auth, email, password)
+            signInWithEmailAndPassword(auth, email, password)
               .then((userCredential) => {
                 const user = userCredential.user;
-                // localStorage.setItem("USER_ID", user.uid);
 
-                set(ref(database, "users/" + user.uid), {
-                  email: email,
-                  password: password,
+                var lgDate = new Date();
+                update(ref(database, "users/" + user.uid), {
+                  last_login: lgDate,
                 });
-
                 navigate("/game");
               })
               .catch((error) => {
                 const errorCode = error.code;
-                const errorMessage = error.message;
-                switch (errorCode) {
-                  case "auth/invalid-email":
-                    alert("Please enter a valid email.");
-                    break;
-                  case "auth/weak-password":
-                    alert(
-                      "Please enter a stronger password, with a minimum of 6 characters."
-                    );
-                    break;
-                  default:
-                    alert("Please enter a valid email and password.");
+                if (errorCode === "auth/user-not-found") {
+                  createUserWithEmailAndPassword(auth, email, password)
+                    .then((userCredential) => {
+                      const user = userCredential.user;
+                      set(ref(database, "users/" + user.uid), {
+                        email: email,
+                        password: password,
+                      });
+
+                      navigate("/game");
+                    })
+                    .catch((error) => {
+                      const errorCode = error.code;
+                      const errorMessage = error.message;
+                      switch (errorCode) {
+                        case "auth/invalid-email":
+                          alert("Please enter a valid email.");
+                          break;
+                        case "auth/weak-password":
+                          alert(
+                            "Please enter a stronger password, with a minimum of 6 characters."
+                          );
+                          break;
+                        default:
+                          alert("Please enter a valid email and password.");
+                      }
+                      console.log(errorMessage);
+                    });
                 }
-                console.log(errorMessage);
               });
           }}
         >
